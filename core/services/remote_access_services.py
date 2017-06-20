3b0a4a85af4a056caea1861a38c929414292f66b
prasanna08 <prasannapatil08@gmail.com>
2017-06-20 09:32:30 +0530

    Fix sorted.

--- core/services/remote_access_services.py
+++ core/services/remote_access_services.py
@@ -68,7 +68,7 @@ def generate_signature(data):
     Returns:
         str. The digital signature generated from request data.
     """
-    msg = json.dumps(sorted(data))
+    msg = json.dumps({key: data[key] for key in sorted(data)})
     key = _get_shared_secret()
 
     # Generate signature and return it.
