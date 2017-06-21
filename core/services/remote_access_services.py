c62e3f87a31a9fa249ee45739e5e602be82ce62c
Prasanna Patil <prasannapatil08@gmail.com>
2017-06-21 22:33:13 +0530

    Sort the dict before generating signature. (#16)
    * Sort the dict before generating signature.

--- core/services/remote_access_services.py
+++ core/services/remote_access_services.py
@@ -68,7 +68,7 @@ def generate_signature(data):
     Returns:
         str. The digital signature generated from request data.
     """
-    msg = json.dumps(data)
+    msg = json.dumps(data, sort_keys=True)
     key = _get_shared_secret()
 
     # Generate signature and return it.
