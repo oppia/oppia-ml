b57e9a760a3fc0f3444183317dfc906c0c26834f
prasanna08 <prasannapatil08@gmail.com>
2017-08-29 06:56:43 +0530

    Make Fetch next job post request and change request data to include payload.

--- core/services/remote_access_services.py
+++ core/services/remote_access_services.py
@@ -89,11 +89,14 @@ def fetch_next_job_request():
 
     payload = {
         'vm_id': _get_vm_id(),
-        'message': ''
+        'message': _get_vm_id(),
     }
     signature = generate_signature(payload['message'])
     payload['signature'] = signature
-    response = requests.get(request_url, params=payload)
+    data = {
+        'payload': json.dumps(payload)
+    }
+    response = requests.post(request_url, data=data)
     return utils.parse_data_received_from_server(response.text)
 
 
@@ -129,7 +132,10 @@ def store_trained_classifier_model(job_result_dict):
     }
     signature = generate_signature(payload['message'])
     payload['signature'] = signature
+    data = {
+        'payload': json.dumps(payload)
+    }
     request_url = "%s:%s/%s" % (
         _get_url(), _get_port(), vmconf.STORE_TRAINED_CLASSIFIER_MODEL_HANDLER)
-    response = requests.post(request_url, json=payload)
+    response = requests.post(request_url, data=data)
     return response.status_code
